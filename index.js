cf1cdd7a60e326aba15eeddec560e92f66d785f6
aadarsh-010 <bvmadarsh@gmail.com>
2024-06-15 00:32:45 +0530

    setup basic server in backend

---
+++ index.js
@@ -0,0 +1,17 @@
+import dotenv from "dotenv";
+import app from "./src/app.js";
+
+
+
+dotenv.config({path:"./config.env"});
+// dotenv.config();
+// const declarations
+console.log(process.env.PORT)
+const PORT = process.env.PORT ;
+
+
+
+
+app.listen(PORT , ()=>{
+    console.log(`serer running on port = ${PORT}`);
+})
---
+++ src/app.js
@@ -0,0 +1,11 @@
+import express from "express";
+const app = express();
+
+
+
+
+app.get("/",(req,res)=>{
+    res.send("hey there");
+});
+
+export default app;
