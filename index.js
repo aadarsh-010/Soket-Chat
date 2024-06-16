69b497499dcc9188ab56e9d473151109993db24b
aadarsh-010 <bvmadarsh@gmail.com>
2024-06-16 13:44:42 +0530

    added middlewares

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
-import dotenv from "dotenv";
+
 import app from "./src/app.js";
 
 
 
-dotenv.config({path:"./config.env"});
-// dotenv.config();
-// const declarations
-console.log(process.env.PORT)
+// console.log(process.env.PORT)
 const PORT = process.env.PORT ;
 
 
--- src/app.js
+++ src/app.js
@@ -1,5 +1,48 @@
 import express from "express";
 const app = express();
+import dotenv from "dotenv";
+dotenv.config({path:"./config.env"});
+import morgan from "morgan";
+import helmet from "helmet";
+import mongoSanitize from "express-mongo-sanitize";
+import cookieParser from "cookie-parser";
+import compression from "compression";
+import fileUpload from "express-fileupload";
+import cors from "cors";
+
+//morgan
+if (process.env.NODE_ENV !== "production") {
+    app.use(morgan("dev"));
+  }
+  
+  //helmet
+  app.use(helmet());
+  
+  //parse json request url
+  app.use(express.json());
+  
+  //parse json request body
+  app.use(express.urlencoded({ extended: true }));
+  
+  //sanitize request data
+  app.use(mongoSanitize());
+  
+  //enable cookie parser
+  app.use(cookieParser());
+
+
+  //gzip compression
+app.use(compression());
+
+//file upload
+app.use(
+  fileUpload({
+    useTempFiles: true,
+  })
+);
+
+//cors
+app.use(cors());
 
 
 
