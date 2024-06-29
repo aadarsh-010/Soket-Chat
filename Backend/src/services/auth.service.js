3b6db123b6f5edd960da23012cf0c3c18fc315e2
aadarsh-010 <bvmadarsh@gmail.com>
2024-06-29 17:57:20 +0530

    backend authentication setup login logout

---
+++ Backend/src/services/auth.service.js
@@ -0,0 +1,81 @@
+import createHttpError from "http-errors";
+import validator from "validator";
+import bcrypt from "bcrypt";
+import { UserModel } from "../models/index.js";
+
+
+export const createUser = async (userData) => {
+  const { name, email, picture, status, password } = userData;
+
+  
+  if (!name || !email || !password) {
+    throw createHttpError.BadRequest("Please fill all fields.");
+  }
+
+  if (
+    !validator.isLength(name, {
+      min: 2,
+      max: 25,
+    })
+  ) {
+    throw createHttpError.BadRequest(
+      "Plase make sure your name is between 2 and 16 characters."
+    );
+  }
+
+
+  if (status && status.length > 64) {
+    throw createHttpError.BadRequest(
+      "Please make sure your status is less than 64 characters."
+    );
+  }
+
+  if (!validator.isEmail(email)) {
+    throw createHttpError.BadRequest(
+      "Please make sure to provide a valid email address."
+    );
+  }
+
+  const checkDb = await UserModel.findOne({ email });
+  if (checkDb) {
+    throw createHttpError.Conflict(
+      "Please try again with a different email address, this email already exist."
+    );
+  }
+
+  if (
+    !validator.isLength(password, {
+      min: 6,
+      max: 128,
+    })
+  ) {
+    throw createHttpError.BadRequest(
+      "Please make sure your password is between 6 and 128 characters."
+    );
+  }
+
+  
+  const user = await new UserModel({
+    name,
+    email,
+    picture: picture || "https://res.cloudinary.com/dkd5jblv5/image/upload/v1675976806/Default_ProfilePicture_gjngnb.png",
+    status: status || "Hey there im using soket-chat",
+    password,
+  }).save();
+
+  return user;
+};
+
+export const signUser = async (email, password) => {
+    const user = await UserModel.findOne({ email: email.toLowerCase() }).lean();
+  
+    //check if user exist
+    if (!user) throw createHttpError.NotFound("Invalid credentials.");
+  
+    //compare passwords
+    let passwordMatches = await bcrypt.compare(password, user.password);
+  
+    if (!passwordMatches) throw createHttpError.NotFound("Invalid credentials.");
+  
+    return user;
+  };
