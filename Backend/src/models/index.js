6140f945af1685493c76f61b3139cf7c3c6d4f10
aadarsh-010 <bvmadarsh@gmail.com>
2024-07-02 05:00:39 +0530

    messeges backend setup made

--- Backend/src/models/index.js
+++ Backend/src/models/index.js
@@ -1,6 +1,4 @@
 import UserModel from "./userModel.js";
 import ConversationModel from "./conversationModel.js";
-// import MessageModel from "./messageModel.js";
-export { UserModel, ConversationModel
-    //  ,MessageModel
-     };
+import MessageModel from "./messageModel.js";
+export { UserModel, ConversationModel,MessageModel};
--- Backend/src/routes/index.js
+++ Backend/src/routes/index.js
@@ -1,10 +1,12 @@
 import express from "express";
 import ConversationRoutes from "./conversation.route.js";
 import authRoutes from "./auth.route.js";
+import MessageRoutes from "./message.route.js";
 const router = express.Router();
 
 
 
 router.use("/auth", authRoutes);
+router.use("/message", MessageRoutes);
 router.use("/conversation", ConversationRoutes);
 export default router;
